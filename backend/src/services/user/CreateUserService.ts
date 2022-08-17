9a43bbc2d5ab897450701f3c1081cb1be5e4abe3
thiagobrsouza <thiago.glespaul@gmail.com>
2022-08-17 14:49:28 -0300

    Create a user in database

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma';
+
 interface UserRequest {
     name: string;
     email: string;
@@ -6,8 +8,20 @@ interface UserRequest {
 
 class CreateUserService {
     async execute({name, email, password}: UserRequest) {
-        console.log(name);
-        return { name: name }
+        if (!email) {
+            throw new Error('Email incorrect');
+        }
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: { email: email }
+        })
+        if (userAlreadyExists) {
+            throw new Error('User already exists');
+        }
+        const user = await prismaClient.user.create({
+            data: { name: name, email: email, password: password },
+            select: { id: true, name: true, email: true }
+        })
+        return user;
     }
 }
 
