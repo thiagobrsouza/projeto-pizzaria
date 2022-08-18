73f6c2353c41e7485f2074af733b33413b50ef1e
thiagobrsouza <thiago.glespaul@gmail.com>
2022-08-18 08:57:48 -0300

    Create category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
@@ -12,6 +13,11 @@ const router = Router();
 router.post('/users', new CreateUserController().handle);
 router.post('/session', new AuthUserController().handle);
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+/**
+ * Category routes
+ */
+router.post('/categories', isAuthenticated, new CreateCategoryController().handle);
 
 export { router };
 
