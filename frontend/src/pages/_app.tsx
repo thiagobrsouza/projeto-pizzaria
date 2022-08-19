632f40944a3e5c772cf7db35886c46966b4b3c9a
thiagobrsouza <thiago.glespaul@gmail.com>
2022-08-19 16:34:03 -0300

    Create a context auth

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from 'next/app'
 import '../../styles/globals.scss'
+import { AuthProvider } from '../contexts/AuthContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,12 +2,26 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
+import { FormEvent, useContext } from 'react'
 import logoImg from '../../public/logo.svg'
 import styles from '../../styles/Home.module.scss'
 import { Button } from '../components/ui/Button'
 import { Input } from '../components/ui/Input'
+import { AuthContext } from '../contexts/AuthContext'
 
 const Home: NextPage = () => {
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+    let data = {
+      email: 'thiago@email.com',
+      password: '12345'
+    }
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -16,7 +30,7 @@ const Home: NextPage = () => {
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Sujeito pizzaria" />
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input placeholder="Digite seu e-mail" type="text" />
             <Input placeholder="Digite sua senha" type="password" />
             <Button type="submit" loading={false}>Acessar</Button>
